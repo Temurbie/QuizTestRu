2e2f5c514c3e1e90ce1b60af206161095b06a844
temurbie <temurbie@gmail.com>
2026-04-25 12:27:56 +0500

    quiztestru

---
+++ src/app/data/authData.ts
@@ -0,0 +1,78 @@
+export interface IAuth{
+    id: number
+    login: string,
+    parol: string,
+    ban: boolean
+}
+
+export const AUTHDATA: IAuth[] = [
+    {
+        id : 1,
+        ban : false,
+        login : "Nilufar",
+        parol: 'sobaka'
+
+    },
+    {
+        id: 2,
+        ban: false,
+        login : "admin",
+        parol: 'admin1'
+    },
+    {
+        id: 3,
+        ban: false,
+        login : 'Ishonch01',
+        parol: 'I0101Ishonch'
+    },
+      {
+        id: 4,
+        ban: false,
+        login : 'Gavhar01',
+        parol: 'Gavhar2101'
+    },
+      {
+        id: 5,
+        ban: false,
+        login : 'Diyorbek01',
+        parol: 'Diyorbek2101'
+    },
+      {
+        id: 6,
+        ban: false,
+        login : 'SunShine',
+        parol: 'SunShine2101'
+    },
+    {
+        id: 7,
+        ban: false,
+        login : 'Dilnoza',
+        parol: 'Dilnoza210101'
+    },
+     {
+        id: 8,
+        ban: false,
+        login : 'Zahro',
+        parol: '14741512'
+    },
+       {
+        id: 9,
+        ban: false,
+        login : 'Soliha',
+        parol: '444565'
+    },
+    {
+        id: 10,
+        ban: false,
+        login : 'Shahzoda',
+        parol: '956896Sh'
+    },
+     {
+        id: 11,
+        ban: false,
+        login : 'Mohinur',
+        parol: '555455M'
+    },
+   
+
+]
