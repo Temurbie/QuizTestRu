702d4a9d6984ae0526e85d84cdc2a32be1d60219
temurbie <temurbie@gmail.com>
2026-04-25 15:18:17 +0500

    .

--- src/app/data/authData.ts
+++ src/app/data/authData.ts
@@ -12,5 +12,34 @@ export const AUTHDATA: IAuth[] = [
         login : 'admin',
         parol: '777admin'
     },
-
+        {
+        id: 2,
+        ban: false,
+        login : 'Irina',
+        parol: '757456irina'
+    },
+      {
+        id: 3,
+        ban: false,
+        login : 'Naima',
+        parol: '99978naima'
+    },
+      {
+        id: 4,
+        ban: false,
+        login : 'Darixa',
+        parol: '12453darixa'
+    },
+     {
+        id: 5,
+        ban: false,
+        login : 'Ksenya',
+        parol: '563154ksenya'
+    },
+    {
+        id: 6,
+        ban: false,
+        login : 'Albina',
+        parol: '11124albina'
+    },
 ]
